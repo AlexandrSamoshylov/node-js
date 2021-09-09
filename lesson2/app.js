d914b87f42d81577bd1420f3a745e153e978357b
A.Samoshylov <shyriks78@gmail.com>
2021-09-09 22:16:49 +0300

    first commit;

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const expressHbs = require('express-handlebars');
+const path = require('path');
 
 
 const {PORT} = require('./configs/config');
 const users = require('./dataBase/users');
-const path = require("path");
 
 const app = express();
+const staticPath = path.join(__dirname, 'static');
+  
 
-app.set('view engine', '.hbs');
+
+app.use(express.static(staticPath));
+app.set('view engine', '.hds');
 app.engine('.hbs', expressHbs({ defaultLayout: false }));
-spp.set('views', path.join(__dirname, 'static'));
+app.set('views', staticPath);
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/ping', (req, res) => {
-  });
+app.get('/ping', (req, res) => {  });
 app.get('/users', (req, res) => {
     res.json(users);
 });
@@ -33,6 +36,13 @@ app.get('/users/:user_id', (req, res) => {
 
     });
 
+// Render endpoints
+app.get('/login', (req, res) => {
+    res.render('login');
+
+})
+
 let server = app.listen(PORT, () => {
     console.log('App listen', PORT)
 });
+
